5f4b40f44653e2688be27736cde4b3195a7b7643
Robert <robbiegabbie@gmail.com>
2019-09-22 20:32:17 -0700

    Changed Connector to a class from enum and fixed my botching of PowerHub

--- src/Dimmer.java
+++ src/Dimmer.java
@@ -1,2 +1,4 @@
 public class Dimmer {
+	int voltage;
+
 }
--- src/PowerHub.java
+++ src/PowerHub.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class PowerHub {
-    boolean dimmer;
-    int circuits;
-    int voltage;
-    ArrayList<Dimmer> cards;
-    ArrayList<Connector> input;
-    ArrayList<Connector> output;
+	boolean dimmer;
+	int circuits;
+	int voltage;
+	ArrayList<Dimmer> cards;
+	ArrayList<Connector> input;
+	ArrayList<Connector> output;
 
 }
